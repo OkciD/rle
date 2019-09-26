802f6ff65ea15201496667206375b6f4e13babb2
Yury Baykov <y.baykov@corp.mail.ru>
2019-09-27 00:44:20 +0300

    trimming \n, left from fgets

--- rle/main.c
+++ rle/main.c
@@ -6,6 +6,18 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include "rle.h"
+
+char *trimNewLine(char *string) {
+	unsigned long length = strlen(string);
+	
+	if (string[length - 1] == '\n') {
+		string[length - 1] = '\0';
+	}
+	
+	return string;
+}
 
 int main(int argc, const char * argv[]) {
 	char string[256];
@@ -14,6 +26,8 @@ int main(int argc, const char * argv[]) {
 	if (!fgets(string, sizeof(string), stdin)) {
 		return 1;
 	}
+	
+	rle(trimNewLine(string));
 	
 	printf("%s\n", string);
 	
