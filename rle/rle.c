d9efe5ae110f378a5f48bb67b177df51193ae377
Yury Baykov <y.baykov@corp.mail.ru>
2019-09-27 00:43:53 +0300

    initial rle func

---
+++ rle/rle.c
@@ -0,0 +1,18 @@
+//
+//  rle.c
+//  rle
+//
+//  Created by y.baykov on 27/09/2019.
+//
+
+#include <string.h>
+#include <stdio.h>
+#include "rle.h"
+
+char *rle(char *string) {
+    unsigned long length = strlen(string);
+    
+    printf("%lu", length);
+    
+    return "";
+}
---
+++ rle/rle.h
@@ -0,0 +1,13 @@
+//
+//  rle.h
+//  rle
+//
+//  Created by y.baykov on 27/09/2019.
+//
+
+#ifndef rle_h
+#define rle_h
+
+char *rle(char *string);
+
+#endif /* rle_h */
