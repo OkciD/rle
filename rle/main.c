dc996b65942cc0ffce9065568f123443b8fba462
Yury Baykov <y.baykov@corp.mail.ru>
2019-09-27 00:16:59 +0300

    reading from console & outputting the result

--- rle/main.c
+++ rle/main.c
@@ -8,7 +8,14 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+	char string[256];
+	
+	printf("Insert string:\n");
+	if (!fgets(string, sizeof(string), stdin)) {
+		return 1;
+	}
+	
+	printf("%s\n", string);
+	
     return 0;
 }
