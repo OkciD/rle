ff78efd1854606d1948ca70ccd3c8634f280e71e
Yury Baykov <y.baykov@corp.mail.ru>
2019-09-27 12:42:08 +0300

    seems like it works

--- rle/rle.c
+++ rle/rle.c
@@ -7,22 +7,30 @@
 
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "rle.h"
 
 void rle(const char * const string, char *result) {
 	size_t currentResultPosition = 0;
-	unsigned int nRepeatings = 0;
+	unsigned int nRepeatings = 1;
 	size_t currentCharIndex = 0;
 	unsigned long length = strlen(string);
+	char buffer[16];
 	
-	for (size_t i = 0; i <= length; ++i) {
+	for (size_t i = 1; i <= length; ++i) {
 		if (string[currentCharIndex] == string[i]) {
 			nRepeatings++;
 		} else {
 			result[currentResultPosition++] = string[currentCharIndex];
 			
+			if (nRepeatings > 1) {
+				sprintf(buffer, "%d", nRepeatings);
+				strcat(result, buffer);
+				currentResultPosition += strlen(buffer);
+			}
+
 			currentCharIndex = i;
-			nRepeatings = 0;
+			nRepeatings = 1;
 		}
 	}
 	
